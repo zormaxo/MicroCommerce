2c051dd0159aac5b96eec376732861c926d547a5
zormax <okocadere@gmail.com>
2023-02-17 16:01:07 +0300

    rabbitmq added

---
+++ src/BuildingBlocks/EventBus/EventBusFactory/EventBusFactory.cs
@@ -0,0 +1,16 @@
+﻿using EventBus;
+using EventBus.Abstraction;
+
+namespace EventBusFactory;
+
+public static class EventBusFactory
+{
+    public static IEventBus Create(EventBusConfig config, IServiceProvider serviceProvider)
+    {
+        return config.EventBusType switch
+        {
+            EventBusType.AzureServiceBus => new EventBusServiceBus.EventBusServiceBus(config, serviceProvider),
+            _ => new EventBusRabbitMQ.EventBusRabbitMQ(config, serviceProvider),
+        };
+    }
+}
